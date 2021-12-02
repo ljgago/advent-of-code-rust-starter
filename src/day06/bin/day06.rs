4069e94bff4ac688f808eaf1e77f223bed2db120
Leonardo Gago <ljgago@gmail.com>
2021-12-02 16:50:05 -0300

    fix: change include_bytes! by include_str!

--- src/day06/bin/day06.rs
+++ src/day06/bin/day06.rs
@@ -4,7 +4,7 @@ mod part1;
 mod part2;
 
 fn main() {
-    let _input = include_bytes!("../input.txt");
+    let _input = include_str!("../input.txt");
 
     println!("--- Part One ---");
     println!("Result: {}", part1::result().unwrap());
