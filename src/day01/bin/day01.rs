4dd448bac7efd525ecee4115bc5066019b297601
Santiago Santana <santana.santiago@gmail.com>
2023-12-01 20:02:56 -0300

    feat: add input for day01

--- src/day01/bin/day01.rs
+++ src/day01/bin/day01.rs
@@ -7,8 +7,8 @@ fn main() {
     let _input = include_str!("../input.txt");
 
     println!("--- Part One ---");
-    println!("Result: {}", part1::part1());
+    println!("Result: {}", part1::part1(_input));
 
     println!("--- Part Two ---");
-    println!("Result: {}", part2::part2());
+    println!("Result: {}", part2::part2(_input));
 }
